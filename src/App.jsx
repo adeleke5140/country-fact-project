95242a8910da20dac4d82141de5172b5cac90a40
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-03-09 18:20:03 +0100

    add localstorage for theme and add country filter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import './App.css'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import CountryList from './components/CountryList'
 import Loader from './components/Loader'
-import useLocalStorage from "use-local-storage"
+import useLocalStorage from 'use-local-storage'
 
 function App() {
   const [countries, setCountries] = useState(null)
   const [isPending, setIsPending] = useState(true)
   const [input, setInput] = useState('')
   const [filteredCountry, setFilteredCountry] = useState([])
+  const [continent, setContinent] = useState('')
+  const [filteredContinent, setFilteredContinent] = useState([])
 
   let isActive = true
 
@@ -20,6 +22,7 @@ function App() {
       try {
         const response = await axios.get('https://restcountries.com/v3.1/all')
         const data = response.data
+        console.log(data)
         //get the first six object
         setCountries(data)
         setIsPending(false)
@@ -38,10 +41,8 @@ function App() {
   //search API data
   function searchItems(searchValue) {
     setInput(searchValue)
-    console.log(searchValue)
     if (setInput !== '') {
       const filteredData = countries.filter((item) => {
-        console.log(item);
         return Object.values(item.name.common)
           .join('')
           .toLowerCase()
@@ -53,9 +54,36 @@ function App() {
     }
   }
 
+  const getCountry = (selectedContinent) => {
+    setContinent(selectedContinent)
+  }
+
+  const isFirstRender = useRef(true)
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false
+      return
+    }
+
+    if (continent !== '') {
+      const filteredCountryByContinent = countries.filter((item) => {
+        return Object.values(item)
+          .join('')
+          .toLowerCase()
+          .includes(continent.toLowerCase())
+      })
+      setFilteredContinent(filteredCountryByContinent)
+    } else {
+      setFilteredContinent(countries)
+    }
+  }, [continent])
+
   //dark mode toggling for the app
-  const defaultDark = window.matchMedia('(prefers-color-scheme:dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? "dark" : 'light');
+  const defaultDark = window.matchMedia('(prefers-color-scheme:dark)').matches
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light',
+  )
 
   const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light'
@@ -64,20 +92,25 @@ function App() {
 
   return (
     <div className="App" data-theme={theme}>
-      <Header theme={theme} switchTheme={switchTheme}/>
-      <Nav input={input} getInput={searchItems} />
+      <Header theme={theme} switchTheme={switchTheme} />
+      <Nav
+        input={input}
+        getInput={searchItems}
+        continent={continent}
+        getCountry={getCountry}
+      />
       {isPending && <Loader />}
       {countries && (
         <CountryList
           countries={countries}
           filteredCountry={filteredCountry}
+          filteredContinent={filteredContinent}
           input={input}
+          continent={continent}
         />
       )}
     </div>
   )
 }
-{
-  /* <h3 style={{display:"grid",placeItems:"center"}}>Loading Countries...</h3> */
-}
+
 export default App
