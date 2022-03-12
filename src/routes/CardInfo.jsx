81711816cc5ca86c33baeae0f76d80fd87bacb81
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-03-13 00:14:39 +0100

    add the cardinfo component

--- src/routes/CardInfo.jsx
+++ src/routes/CardInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import '../App.css'
 import ReturnButton from '../components/ReturnButton'
+import CountryDetails from '../components/CountryDetails'
 
 export default function cardInfo({ theme }) {
   const location = useLocation()
@@ -10,6 +11,7 @@ export default function cardInfo({ theme }) {
     <div className="App">
       <div className="cardInfo">
         <ReturnButton buttonText="Back" theme={theme} />
+        <CountryDetails country={country} />
       </div>
     </div>
   )
