64f1264b3dcadffeb38f13ac1c2afcbea494d1ad
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-03-13 00:13:58 +0100

    add extra conditional syntax to countryInfo component

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -1,19 +1,29 @@
-export default function CountryInfo({ country }) {
+export default function CountryInfo({ country, subRegion, nativeName }) {
   let countryPopulation = country.population
     .toString()
     .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,')
-
+  console.log(subRegion, nativeName)
   return (
     <div className="country-info">
+      {nativeName && (
+        <p className="country-region">
+          <span className="title">Native Name:</span> {nativeName}
+        </p>
+      ) }
       <h2 className="country-name"> {country.name.common}</h2>
       <p className="country-population">
-        <b>Population:</b> {countryPopulation}
+        <span className="title">Population:</span> {countryPopulation}
       </p>
       <p className="country-region">
-        <b>Region:</b> {country.region}
+        <span className="title">Region:</span> {country.region}
       </p>
+      {subRegion && (
+        <p className="country-region">
+          <span className="title">SubRegion:</span> {subRegion}
+        </p>
+      ) }
       <p className="country-capital">
-        <b>Capital:</b> {country.capital}
+        <span className="title">Capital:</span> {country.capital}
       </p>
     </div>
   )
